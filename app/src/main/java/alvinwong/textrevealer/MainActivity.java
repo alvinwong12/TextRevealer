98be2e6c4bb7e824d664a763d863f6e0940d70ab
alvinwong12 <alvinwong312@gmail.com>
2018-03-05 23:51:26 -0500

    camera permission request

--- app/src/main/java/alvinwong/textrevealer/MainActivity.java
+++ app/src/main/java/alvinwong/textrevealer/MainActivity.java
@@ -1,13 +1,57 @@
 package alvinwong.textrevealer;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.Snackbar;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.vision.text.Line;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    private static final int RC_HANDLE_CAMERA_PERM = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (rc == PackageManager.PERMISSION_GRANTED) {
+            // createCameraSource(autoFocus, useFlash);
+            Log.d(TAG, "Camera permission granted");
+        } else {
+            requestCameraPermission();
+        }
+    }
+    private void requestCameraPermission() {
+        Log.w(TAG, "Camera permission is not granted. Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+            return;
+        }
+
+        final Activity thisActivity = this;
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(thisActivity, permissions,
+                        RC_HANDLE_CAMERA_PERM);
+            }
+        };
+        Snackbar.make((LinearLayout)findViewById(R.id.layout), R.string.permission_camera_rationale,
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.ok, listener)
+                .show();
     }
 }
