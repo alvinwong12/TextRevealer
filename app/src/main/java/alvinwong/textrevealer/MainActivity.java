1481d2a835c8b8f290302c771e779cdd7d469308
alvinwong12 <alvinwong312@gmail.com>
2018-03-07 23:24:33 -0500

    button to show camera and allow to take picture

--- app/src/main/java/alvinwong/textrevealer/MainActivity.java
+++ app/src/main/java/alvinwong/textrevealer/MainActivity.java
@@ -9,25 +9,44 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.Snackbar;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.graphics.Bitmap;
 
 import com.google.android.gms.vision.text.Line;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final int RC_HANDLE_CAMERA_PERM = 2;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView mImageView;
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mImageView = (ImageView) findViewById(R.id.mImageView);
+        mButton = (Button) findViewById(R.id.mButton);
         int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
         if (rc == PackageManager.PERMISSION_GRANTED) {
             // createCameraSource(autoFocus, useFlash);
             Log.d(TAG, "Camera permission granted");
+            // takePicture();
         } else {
             requestCameraPermission();
         }
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
     }
     private void requestCameraPermission() {
         Log.w(TAG, "Camera permission is not granted. Requesting permission");
@@ -49,9 +68,24 @@ public class MainActivity extends AppCompatActivity {
                         RC_HANDLE_CAMERA_PERM);
             }
         };
-        Snackbar.make((LinearLayout)findViewById(R.id.layout), R.string.permission_camera_rationale,
+        Snackbar.make((RelativeLayout)findViewById(R.id.layout), R.string.permission_camera_rationale,
                 Snackbar.LENGTH_INDEFINITE)
                 .setAction(R.string.ok, listener)
                 .show();
+    }
+
+    private void takePicture(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
